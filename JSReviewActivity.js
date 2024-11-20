fe80eeb269fc87686ccebdda3dbe1c9dbd625666
Your Usernamemspivey <your@email.comspiveymonae@gmail.com>
2024-11-19 22:21:55 -0500

    initial commit

---
+++ JSReviewActivity.js
@@ -0,0 +1,54 @@
+​​/*How do we assign a value to a variable?
+use Let variableName = value
+Const variableName = value
+
+
+How do we change the value of a variable?
+reassign it using let and the = operator
+
+
+How do we assign an existing variable to a new variable?
+equal the new variable to the existing variable
+
+
+Remind me, what are declare, assign, and define?
+Declare: reserves a name for the variable - uses let and const
+Assign: sets the value of the variable using operator =
+Define: like declaring-  implies assigning an initial value
+
+
+What is pseudocoding and why should you do it?
+helps plan out your coding logic using programming syntax but it's not executable
+
+
+What percentage of time should be spent thinking about
+how you're going to solve a problem vs actually typing
+in code to solve it?
+20-30% planning and 70-80% coding */
+
+
+//B.
+let firstVariable = "Hello World"
+   console.log(firstVariable)
+
+
+let firstVariable = 22
+   console.log(firstVariable)
+
+
+let firstVariable = 22  
+let secondVariable = firstVariable
+   console.log(secondVariable)
+
+
+let secondVariable = "Hungry"
+   console.log(secondVariable)
+
+
+// the value of firstVariable is 22. Confirmed via console.log(firstVariable)
+
+
+let yourName = "Monae Ann"
+   console.log(yourName)
+
+
